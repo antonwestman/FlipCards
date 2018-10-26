148f8643f4cda08ddb0481efdd83dabaac75c5ba
antonwestman <anton.westman@mejsla.se>
2018-10-27 00:59:29 +0200

    Flex those views

--- containers/PointAtAnimals/PointAtAnimals.js
+++ containers/PointAtAnimals/PointAtAnimals.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import _ from 'lodash';
 import {
+  Dimensions,
   Image, 
   Modal,
-  StyleSheet, 
+  Platform, 
   Text, 
   TouchableOpacity, 
   View
@@ -31,6 +32,7 @@ class PointAtAnimals extends React.Component {
       sampleAnimals: animals,
       targetAnimal: target,
     }
+    this.navbarHight = this.getNavBarHeight()
   }
 
   nextAnimal(){
@@ -66,94 +68,207 @@ class PointAtAnimals extends React.Component {
     await soundObject.playAsync();
   }
 
-  sampleAnimals = () => _.sampleSize(animals, 15)
+  sampleAnimals = () => _.sampleSize(animals, 12)
 
   targetAnimal = () => _.sample(this.sampleAnimals())
+
+  getNavBarHeight() {
+    if (Platform.OS == 'ios') {
+
+        let d = Dimensions.get('window');
+        const { height, width } = d;
+        
+        if(height === 812 || width === 812)
+            return 88 // iPhone X navbar height (regular title);
+        else
+            return 64 // iPhone navbar height;
+    } else
+        return 54 //android portrait navbar height;
+  }
 
   render() {
     const { t, i18n } = this.props;
     const { sampleAnimals, targetAnimal, modalAnimal } = this.state
     return (
-      <View style={styles.container}>
-        { modalAnimal &&
-          <Modal
-            animationType="slide"
-            transparent={false}
-            >
-            <View style={{marginTop: 22}}>
-              <View style={styles.modal}>
-                <TouchableOpacity onPress={ () =>
-                  this.playSound(localAnimalWord(modalAnimal.species))}>
-                  <Text style={styles.titleText}>{_.upperFirst(t(`${modalAnimal.species}`))}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {
-                    this.playSound(_.sample(animalSounds[modalAnimal.species]));
-                }}>
-                <Image source={modalAnimal.img}
-                       style={styles.modalImage}/>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  accessibilityLabel={t('next_animal')}
-                  onPress={() => {
-                    this.nextAnimal();
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'stretch',
+        }}>
+          { modalAnimal &&
+            <Modal
+              animationType="fade"
+              transparent={false}
+              onRequestClose={() => {
+                  this.nextAnimal();
+                }    
+              }
+              >
+              <View
+                style={{
+                  flex: 1,
+                  marginTop: this.navbarHight
+                }}
+              >
+                <View 
+                  style={{
+                    flex:1,
+                    backgroundColor: 'steelblue',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                  }}>
+                  <TouchableOpacity
+                    onPress={ () =>
+                      this.playSound(localAnimalWord(modalAnimal.species))
+                    }
+                    onLongPress={ () => 
+                      this.playSound(_.sample(animalSounds[modalAnimal.species]))
+                    }
+                  >
+                    <Text style={{letterSpacing: 4, fontSize: 50}}>{_.upperFirst(t(`${modalAnimal.species}`))}</Text>
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={{
+                    flex: 4,
+                    padding:4,
+                    backgroundColor: 'skyblue'
                   }}
                 >
-                  <Text style={styles.titleText}>{_.upperFirst(t('next_animal'))}</Text>
-                </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => {
+                      this.playSound(_.sample(animalSounds[modalAnimal.species]));
+                    }}
+                    onLongPress={ () => 
+                      this.playSound(localAnimalWord(modalAnimal.species))
+                    }
+                  >
+                  <Image 
+                    style={{
+                      margin: 'auto',
+                      width: '100%',
+                      maxHeight:'100%',
+                      borderWidth: 1,
+                      borderRadius: 3
+                    }}
+                    source={modalAnimal.img}/>
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={{
+                    flex: 1,
+                    backgroundColor: 'steelblue',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <TouchableOpacity
+                    accessibilityLabel={t('next_animal')}
+                    onPress={() => {
+                      this.nextAnimal();
+                    }}
+                    style={{
+                      width: '100%',
+                      borderWidth: 2,
+                      borderRadius:2,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: 'skyblue'
+                    }}
+                  >
+                    <Text 
+                      style={{
+                        letterSpacing: 1,
+                        fontSize: 20,
+                        padding: 20,
+                        fontWeight: '300'
+                      }}
+                    >
+                      {_.upperCase(t('next_animal'))}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
-            </View>
-          </Modal>
-        }
-        {
-          sampleAnimals.map(animal => 
-            <TouchableOpacity key={animal['id']} onPress={
-              (e) => {
-                return this.onClick(e, animal)
+            </Modal>
+          }
+          <View
+            style={{
+              flex:1,
+              backgroundColor: 'steelblue',
+              justifyContent: 'center',
+              alignItems: 'center'
+
+            }}
+          >
+            <TouchableOpacity 
+              onPress={ () =>
+                this.playSound(localAnimalWord(targetAnimal.species))
               }
-            }>
-              <Image source={animal['img']}
-                     style={styles.smallImage}/>
+              onLongPress={ () => 
+                this.playSound(_.sample(animalSounds[targetAnimal.species]))
+              }
+            >
+              <Text style={{letterSpacing: 4, fontSize: 50}}>{_.upperFirst(t(`${targetAnimal.species}`))}</Text>
             </TouchableOpacity>
-          )
-        }
-        <TouchableOpacity onPress={ () =>
-          this.playSound(localAnimalWord(targetAnimal.species))}>
-          <Text style={styles.titleText}>{_.upperFirst(t(`${targetAnimal.species}`))}</Text>
-        </TouchableOpacity>
+          </View>
+          <View style={{
+            flex:4,
+            backgroundColor: 'skyblue',
+            padding: 2
+          }}
+            >
+            {
+              _.chunk(sampleAnimals, 3).map(animals =>
+                <View key={animals.map(a => a.id).join('-')} 
+                      style={{
+                        flex: 3,
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'stretch',
+                      }}
+                >
+                  {
+                    animals.map(animal =>
+                      <View key={animal.id} style={{flex: 1}}>
+                        <TouchableOpacity
+                          onPress={
+                            (e) => {
+                              return this.onClick(e, animal)
+                            }
+                          }
+                          onLongPress={ () => 
+                            this.playSound(localAnimalWord(animal.species))
+                          }
+                          style={{
+                            flex:1,
+                            padding: 2
+                          }}
+                        >
+                          <Image style={{
+                            margin: 'auto',
+                            width: '100%',
+                            maxHeight:'100%',
+                            borderWidth: 1,
+                            borderRadius: 3
+                          }} source={animal['img']} />
+                        </TouchableOpacity>
+                      </View>
+                    )
+                  }
+                </View>
+              )
+            }
+          </View>
+          <View
+            style={{
+              flex:1,
+              backgroundColor: 'steelblue'
+            }}
+          />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    height: '100%'
-  },
-  modal: {
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    height: '100%'
-  },
-  titleText: {
-    fontSize: 50,
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
-  smallImage: {
-    width: 110,
-    height: 110,
-    margin: 2,
-  },
-  modalImage: {
-    width: 350,
-    height: 350
-  }
-});
 
 export default withNamespaces(['animals', 'routes'], { wait: true })(PointAtAnimals);
